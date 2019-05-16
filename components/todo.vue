c74c461f8b574d205f3f59d5667e9769fed8ebf2
Nicolas Mercier <dev@nicolasmercier.io>
2019-05-16 15:12:50 +0200

    todos fetch implementation

---
+++ components/todo.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-list-tile>
+    <v-list-tile-action>
+      <v-checkbox v-model="completed"></v-checkbox>
+    </v-list-tile-action>
+    <v-list-tile-content>
+      <v-list-tile-title :class="{'line-through' : todo.completed}">{{todo.title}}</v-list-tile-title>
+    </v-list-tile-content>
+  </v-list-tile>
+</template>
+
+<script>
+  export default {
+    name: "todo",
+    props : ['todo'],
+    data() {
+      return {
+        completed : false
+      }
+    },
+    created() {
+      this.completed = this.todo.completed
+    }
+  }
+</script>
+
+<style scoped>
+  .line-through{
+    text-decoration: line-through;
+  }
+</style>
